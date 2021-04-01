f7421ce756cee1149fcd7e7994e3f5754c931482
JuanRomero11 <juan.romero-si@mail.escuelaing.edu.co>
2021-03-31 23:23:24 -0500

    conexion

--- src/main/java/edu/escuelaing/service4All/backend/repository/ServicioRepository.java
+++ src/main/java/edu/escuelaing/service4All/backend/repository/ServicioRepository.java
@@ -1,14 +1,13 @@
 package edu.escuelaing.service4All.backend.repository;
 
-import java.util.List;
 import edu.escuelaing.service4All.backend.model.*;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
-public interface ServicioRepository  extends JpaRepository<Servicio,Integer> {
+@Repository
+public interface ServicioRepository  extends JpaRepository<Servicio,Long> {
    
 	
 }
