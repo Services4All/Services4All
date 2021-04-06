7381ebbdfbc05803a9bbbfb6dfee999b852bce86
JuanRomero11 <juan.romero-si@mail.escuelaing.edu.co>
2021-04-05 21:36:09 -0500

    :white_check_mark: test17

--- src/main/java/edu/escuelaing/service4All/backend/security/SecurityConfig.java
+++ src/main/java/edu/escuelaing/service4All/backend/security/SecurityConfig.java
@@ -34,8 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-
-        .authorizeRequests().antMatchers("/comentarios/**","/servicio","/servicios","/comentarios").permitAll()
+        .authorizeRequests()
         .anyRequest()
         .authenticated()
         .and()
