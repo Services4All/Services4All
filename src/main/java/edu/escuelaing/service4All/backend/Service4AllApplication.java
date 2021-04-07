b812755819d78de68425b78c9bfa2e542d9b8662
Andres sotelo <andres.sotelo@mail.escuelaing.edu.co>
2021-04-06 19:53:54 -0500

    prueba

--- src/main/java/edu/escuelaing/service4All/backend/Service4AllApplication.java
+++ src/main/java/edu/escuelaing/service4All/backend/Service4AllApplication.java
@@ -3,6 +3,9 @@ package edu.escuelaing.service4All.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Service4AllApplication {
@@ -10,5 +13,13 @@ public class Service4AllApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Service4AllApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }
