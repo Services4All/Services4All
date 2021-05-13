abeb54dac3529cce3e1a0f284923451fa7008ef1
Andres sotelo <andres.sotelo@mail.escuelaing.edu.co>
2021-05-12 21:56:04 -0500

    test final

--- src/main/java/edu/escuelaing/service4All/backend/controller/SuscripcionController.java
+++ src/main/java/edu/escuelaing/service4All/backend/controller/SuscripcionController.java
@@ -1,8 +1,8 @@
 package edu.escuelaing.service4All.backend.controller;
 
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+// import java.util.logging.Level;
+// import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -42,13 +42,13 @@ public class SuscripcionController {
 
     @RequestMapping(value="/addsuscripcion",method = RequestMethod.POST)
     public ResponseEntity<?> addSuscripcion(@RequestBody Suscripcion suscrip){
-        try {
+        // try {
         	suscripcionServices.saveSuscripcion(suscrip);
             return new ResponseEntity<>(HttpStatus.CREATED);
-        } catch (Exception ex) {
-            Logger.getLogger(SuscripcionController.class.getName()).log(Level.SEVERE, null, ex);
-            return new ResponseEntity<>(ex.getMessage(),HttpStatus.FORBIDDEN);
-        }
+        // } catch (Exception ex) {
+        //     Logger.getLogger(SuscripcionController.class.getName()).log(Level.SEVERE, null, ex);
+        //     return new ResponseEntity<>(ex.getMessage(),HttpStatus.FORBIDDEN);
+        // }
 
     }
     
