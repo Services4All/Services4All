b3183e00454922df120a990175a4b58841421371
Andres sotelo <andres.sotelo@mail.escuelaing.edu.co>
2021-05-13 12:40:09 -0500

    Update comentario

--- src/main/java/edu/escuelaing/service4All/backend/controller/ComentarioController.java
+++ src/main/java/edu/escuelaing/service4All/backend/controller/ComentarioController.java
@@ -5,14 +5,13 @@ import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.sql.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -61,6 +60,16 @@ public class ComentarioController {
             Logger.getLogger(ComentarioController.class.getName()).log(Level.SEVERE, null, ex);
             return new ResponseEntity<>(ex.getMessage(),HttpStatus.FORBIDDEN);
         }
+    }
 
+    @PutMapping(value = "/putComentario")
+    public ResponseEntity<?> putComentario(@RequestBody Comentario comentario) {
+        try {
+            comentarioServices.update(comentario);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception ex) {
+            Logger.getLogger(ComentarioController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 }
