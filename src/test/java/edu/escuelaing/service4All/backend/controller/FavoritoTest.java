f65d2f1d6cd3cf1e8079f23b7e7349ff6dfa6137
EstebanK23 <estebanb0203@hotmail.com>
2021-05-18 13:51:51 -0500

    pruebas

--- src/test/java/edu/escuelaing/service4All/backend/controller/FavoritoTest.java
+++ src/test/java/edu/escuelaing/service4All/backend/controller/FavoritoTest.java
@@ -1,6 +1,8 @@
 package edu.escuelaing.service4All.backend.controller;
 
 import edu.escuelaing.service4All.backend.model.Favorito;
+import edu.escuelaing.service4All.backend.model.Suscripcion;
+import edu.escuelaing.service4All.backend.repository.impl.FavoritoRepositoryImpl;
 import edu.escuelaing.service4All.backend.services.FavoritoServices;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -20,6 +23,8 @@ class FavoritoTest {
 
     @Autowired
     FavoritoServices favoritoServices;
+    @Autowired
+    FavoritoRepositoryImpl favoritoRepository;
 
     @Test
     void creacionFavoritoSetYGet() throws Exception {
@@ -39,7 +44,15 @@ class FavoritoTest {
         favoritoServices.addfavorito(nuevo);
         favoritoServices.findAllFavoritos();
         favoritoServices.findFavoritosByIdUsuario(222);
+        favoritoServices.removeFavorito(nuevo);
         assertTrue(true);
+    }
+
+    @Test
+    void deleteSuscription(){
+        Favorito nuevo = new Favorito(222, 222);
+        favoritoRepository.addfavorito(nuevo);
+        favoritoRepository.removeFavorito(nuevo);
     }
 
 }
